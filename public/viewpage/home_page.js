bcfa68d33219ab0100b89b7c01e6364d82f1c8a9
Swathi SampathKumar <ssampathkumar@uco.edu>
2022-10-10 12:54:48 -0500

    initial commit

--- public/viewpage/home_page.js
+++
@@ -1,5 +0,0 @@
-import * as Elements from './elements.js'
-
-export function home_page(){
-    Elements.root.innerHTML = '<h1>Home Page</h1>'
-}
--- public/viewpage/users_page.js
+++
@@ -1,5 +0,0 @@
-import * as Elements from './elements.js'
-
-export function users_page(){
-    Elements.root.innerHTML = '<h1>Users Page</h1>'
-}
